c14c205c1b1d71b490cdfc94802eed516d59d059
rbqja5597 <rbqja5597@naver.com>
2020-12-01 13:49:48 +0900

    원하는 사이트 게시물 가져와서 출력하기

--- src/main/java/com/sbs/selenium/exam/Main.java
+++ src/main/java/com/sbs/selenium/exam/Main.java
@@ -1,11 +1,15 @@
 package com.sbs.selenium.exam;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.nio.Buffer;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -15,9 +19,13 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		printArticle();
+
+	}
+
+	private static void printArticle() {
 		Path path = Paths.get(System.getProperty("user.dir"), "src/main/resources/chromedriver.exe");
-		
+
 		// WebDriver 경로 설정
 		System.setProperty("webdriver.chrome.driver", path.toString());
 
@@ -25,39 +33,99 @@ public class Main {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized"); // 전체화면으로 실행
 		options.addArguments("--disable-popup-blocking"); // 팝업 무시
-		options.addArguments("--disable-default-apps");  // 기본앱 사용안함
-		
+		options.addArguments("--disable-default-apps"); // 기본앱 사용안함
+
 		// WebDriver 객체 생성
 		ChromeDriver driver = new ChromeDriver(options);
-		
+
 		// 빈 탭 생성
-		driver.executeScript("window.open('about:blank', '_blank');");
-		
+		// driver.executeScript("window.open('about:blank', '_blank');");
+
 		List<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-		
+
+		// 첫번째 탭으로 전환
+		driver.switchTo().window(tabs.get(0));
+		driver.get("http://dpg.danawa.com/bbs/list?boardSeq=11");
+
+		Util.sleep(1000);
+
+		List<WebElement> elements = driver
+				.findElements(By.cssSelector("div.article .bbs_list > .bbs_table>.normal_group .title"));
+
+		for (WebElement element : elements) {
+			String title = element.getText().trim();
+			System.out.println(title);
+			
+			
+		}
+
+	}
+
+	private static void downloadImage() {
+
+		Path path = Paths.get(System.getProperty("user.dir"), "src/main/resources/chromedriver.exe");
+
+		// WebDriver 경로 설정
+		System.setProperty("webdriver.chrome.driver", path.toString());
+
+		// WebDriver 옵션 설정
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized"); // 전체화면으로 실행
+		options.addArguments("--disable-popup-blocking"); // 팝업 무시
+		options.addArguments("--disable-default-apps"); // 기본앱 사용안함
+
+		// WebDriver 객체 생성
+		ChromeDriver driver = new ChromeDriver(options);
+
+		// 빈 탭 생성
+		// driver.executeScript("window.open('about:blank', '_blank');");
+
+		List<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+
 		// 첫번째 탭으로 전환
 		driver.switchTo().window(tabs.get(0));
 		driver.get("https://unsplash.com/t/nature");
-		
+
 		File downloadsFolder = new File("downloads");
-		
+
 		if (downloadsFolder.exists() == false) {
 			downloadsFolder.mkdir();
 		}
 
-		
-		List<WebElement> imgElements = driver.findElements(By.cssSelector("[data-test=\"masonry-grid-count-three\"] img"));
-		
+		Util.sleep(1000);
+
+		List<WebElement> imgElements = driver
+				.findElements(By.cssSelector("[data-test=\"masonry-grid-count-three\"] img"));
+
 		for (WebElement imElement : imgElements) {
 			String src = imElement.getAttribute("src");
-			
+			System.out.println(src);
+
 			if (src.contains("images.unsplash.com/photo-") == false) {
 				continue;
 			}
+
+			BufferedImage saveImage = null;
+
+			try {
+				saveImage = ImageIO.read(new URL(src));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			if (saveImage != null) {
+				try {
+					String fileName = src.split("/")[3];
+					fileName = fileName.split("\\?")[0];
+					ImageIO.write(saveImage, "jpg", new File(fileName + "jpg"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println(src);
 		}
-		
-		
-		
+
 	}
 
 }
